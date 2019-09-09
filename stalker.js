a89062b9e829e0cdd45cae4d7b82b746343f064a
supriya <89706.kumar@students.itu.edu>
2019-09-09 15:04:04 -0700

    prompting for name and age

---
+++ stalker.js
@@ -0,0 +1,5 @@
+var first_name = prompt("What is your first name");
+var last_name = prompt ("What is your last name");
+var age = prompt("What is your age");
+console.log("Your full name is " + first_name + last_name);
+console.log("You are " +  age +  "years old");
